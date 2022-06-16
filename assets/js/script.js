9bea95856682865a0ab54e373282739e25b1e520
bebo <ibrahimimran96@gmail.com>
2022-06-15 20:37:15 -0400

    Added functions to check user password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,99 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  alert("Welcome to my password generator! Before we can generate a password, please specify your password criteria: ");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  getUserInputLength();
 
   passwordText.value = password;
+  
 }
 
-
-
-function getUserInputLength(){
+var getUserInputLength = function(){
   var passwordLength = window.prompt('Please input the length of your password (minimum 8 characters, maximum 128 characters): ');
   
-  if(passwordLength < 8 || passwordLength > 128){
+  if(passwordLength < 8 || passwordLength > 128 || passwordLength == '' || isNaN(passwordLength)){
     window.alert("Please type a length within the bounds (minimum 8 characters, maximum 128 characters): ");
     // use return to call it again and stop the rest of this function from running
-    return getUserInput();
+    return getUserInputLength();
   }
+  return passwordLength;
+}
+
+var wantLowerCase = function(){
+
+  var userResponse = window.prompt('Please specify whether you would like to include lowercase characters in your password. Type "Yes" or "No": ');
+  if(userResponse.toUpperCase() === "YES"){
+    console.log("YES");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() === "NO"){
+    console.log("NO");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() !== 'YES' || userResponse.toUpperCase() !== 'NO'){
+    window.alert("Please enter a valid input. ('Yes' or 'No')");
+    return wantLowerCase();
+  }
+
+}
+
+var wantUpperCase = function(){
+
+  var userResponse = window.prompt('Please specify whether you would like to include uppercase characters in your password. Type "Yes" or "No": ');
+  if(userResponse.toUpperCase() === "YES"){
+    console.log("YES");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() === "NO"){
+    console.log("NO");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() !== 'YES' || userResponse.toUpperCase() !== 'NO'){
+    window.alert("Please enter a valid input. ('Yes' or 'No')");
+    return wantUpperCase();
+  }
+
+}
+
+var wantNumeric = function(){
+
+  var userResponse = window.prompt('Please specify whether you would like to include numeric characters in your password. Type "Yes" or "No": ');
+  if(userResponse.toUpperCase() === "YES"){
+    console.log("YES");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() === "NO"){
+    console.log("NO");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() !== 'YES' || userResponse.toUpperCase() !== 'NO'){
+    window.alert("Please enter a valid input. ('Yes' or 'No')");
+    return wantNumeric();
+  }
+
+}
+
+var wantSpecialChars = function(){
+
+  var userResponse = window.prompt('Please specify whether you would like to include special characters in your password. Type "Yes" or "No": ');
+  if(userResponse.toUpperCase() === "YES"){
+    console.log("YES");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() === "NO"){
+    console.log("NO");
+    return userResponse.toUpperCase();
+  }else if(userResponse.toUpperCase() !== 'YES' || userResponse.toUpperCase() !== 'NO'){
+    window.alert("Please enter a valid input. ('Yes' or 'No')");
+    return wantSpecialChars();
+  }
+
 }
 
 function generatePassword(){
-  var password;
-
+  var password = 0;
+  alert("Welcome to my password generator! Before we can generate a password, please specify your password criteria: ");
+  var passLength = getUserInputLength();
+  var lowerCase = wantLowerCase();
+  var upperCase = wantUpperCase();
+  var passNumeric = wantNumeric();
+  var passSpecial = wantSpecialChars();
+  
+  
+  return password;
 }
 
 // Add event listener to generate button
